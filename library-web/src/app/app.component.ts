5604eb04761b5557b809440f4df23d4a53eb053a
CHIMGEE Ganbold <ganbold.chimgee@etu.uca.fr>
2024-12-08 19:22:54 +0100

    création choice-page

--- library-web/src/app/app.component.ts
+++ library-web/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChoicePageComponent } from './components/choice-page/choice-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChoicePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ library-web/src/app/components/choice-page/choice-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ChoicesComponent } from './choices/choices.component';
+
+@Component({
+  selector: 'app-choice-page',
+  imports: [ChoicesComponent],
+  templateUrl: './choice-page.component.html',
+  styleUrl: './choice-page.component.css'
+})
+export class ChoicePageComponent {
+
+}
